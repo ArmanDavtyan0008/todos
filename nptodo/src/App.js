1f020346e4aa3d0091d011a806cbf533747f9e8b
-dav <davtyan.arman1996@gmail.com>
2023-03-09 12:28:29 +0400

    some changes

---
+++ nptodo/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tbody>
+          <tr>
+            <td> aa </td>
+            <td> bbnp </td>
+          </tr>
+        </tbody>
+     </table>
+    </div>
+  );
+}
+
+export default App;
