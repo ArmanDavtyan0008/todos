8fa23c064da84ecdd9ffb69573ecb2fb539f1941
-dav <davtyan.arman1996@gmail.com>
2023-03-13 18:27:05 +0400

    the latest version

--- nptodo/src/Components/Form.js
+++ nptodo/src/Components/Form.js
@@ -4,57 +4,71 @@ import { v4 } from "uuid"
 
 export default class Form extends React.Component {
 
-        state = {
-            value: "",
-            id: v4(),
-            list: [],
+    state = {
+
+        value: "",
+        id: v4(),
+        list: [],
     }
+
     ChangedValue(event) {
+
         this.setState({ value: event.target.value })
-       
+
     }
 
-        AddItem = () => {
-            const NewItem = {
-                id: v4(),
-                NewItem: this.state.value
-            }
-            const list = [...this.state.list]
-            list.push(NewItem)
-            this.setState({ value: "", list: list })
-             
-        }
+    AddItem = () => {
 
-        DeleateItem = (id) => {
-            const list = [...this.state.list]
-            const UpdatedList = list.filter(item => item.id !== id)
-            this.setState({ list: UpdatedList })
+        const NewItem = {
+            id: v4(),
+            NewItem: this.state.value
         }
+        const newList = [...this.state.list]
+        newList.push(NewItem)
+        this.setState({ value: "", list: newList })
 
-        render() {
+    }
+
+    DeleateItem = (id) => {
+        const list = [...this.state.list]
+        const UpdatedList = list.filter(item => item.id !== id)
+        this.setState({ list: UpdatedList })
+
+    }
+
+    liForUlElement = () => {
+        console.log("function was called")
+        const list = this.state.list
+        const newItems = list.map(item => {
             return (
-                <div className="Container" >
-                
-                        <h1> To-DO list </h1>
-                        <input type="text" placeholder="Write Your Todos" onChange={evt => this.ChangedValue(evt)} />
-                        <button onClick={this.AddItem}
-                        > Add  </button>
-                        <br />
-                        <ul>
-                            {this.state.list.map(item => {
-                                return (
-                                    <li key={item.id} > {item.NewItem}
-                                        <button onClick={this.DeleateItem(item.id)}>
-                                            X
-                                        </button>
-                                    </li>
-                                )
-                            })}
-
-                            </ul>
-                       
-                    
-                </div>
+                <li key={item.id}> {item.NewItem}
+                    <button onClick={this.DeleateItem}>
+                        X </button>
+                </li>
             )
-        }
+        })
     }
+
+
+    render() {
+        return (
+            <div className="Container" >
+
+                <h1> To-DO list </h1>
+                <input type="text" placeholder="Write Your Todos" onChange={evt => this.ChangedValue(evt)} />
+                <button onClick={this.AddItem}
+                > Add  </button>
+                <br />
+                <ul>{this.state.list.map(item => {
+                    return (
+                        <li key={item.id}> {item.NewItem}
+                            <button onClick={this.DeleateItem}>
+                                X </button>
+                        </li>
+                    )
+                })} </ul>
+
+            </div>
+        )
+    }
+}
